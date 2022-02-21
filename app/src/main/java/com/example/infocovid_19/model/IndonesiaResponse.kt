393d8c489c76bc78edcf2f8ec4189544c2f855b3
KAMEKID <3103120142@student.smktelkom-pwt.sch.id>
2022-02-22 00:05:36 +0700

    KK4 B-Informasi Covid-19

---
+++ app/src/main/java/com/example/infocovid_19/model/IndonesiaResponse.kt
@@ -0,0 +1,9 @@
+package com.example.infocovid_19.model
+
+data class IndonesiaResponse(
+    val name: String,
+    val positif : String,
+    val sembuh : String,
+    val dirawat : String,
+    val meninggal : String,
+)
